84ee570f73dc57f4bacdcad848b8310dadedab15
Kat <katg@cs.stanford.edu>
2017-03-08 19:01:57 -0800

    Keep embedding size var in one place.

--- code/nli_model.py
+++ code/nli_model.py
@@ -17,7 +17,6 @@ logging.basicConfig(level=logging.INFO)
 class Config:
   ff_hidden_size = 100
   hidden_size = 100
-  embedding_size = 300
   num_classes = 3
   lr = 0.0005
   verbose = False
@@ -94,11 +93,11 @@ class NLISystem(object):
 
     # weight hidden layers before merging
     with tf.variable_scope("Hidden-Weights"):
-      wp = tf.get_variable("Wp", shape=(Config.embedding_size, Config.hidden_size), initializer=tf.contrib.layers.xavier_initializer())
+      wp = tf.get_variable("Wp", shape=(embedding_size, Config.hidden_size), initializer=tf.contrib.layers.xavier_initializer())
       whp = tf.matmul(hp, wp)
       tf.summary.histogram("whp", whp)
 
-      wh = tf.get_variable("Wh", shape=(Config.embedding_size, Config.hidden_size), initializer=tf.contrib.layers.xavier_initializer())
+      wh = tf.get_variable("Wh", shape=(embedding_size, Config.hidden_size), initializer=tf.contrib.layers.xavier_initializer())
       whh = tf.matmul(hh, wh)
       tf.summary.histogram("whh", whh)
 
