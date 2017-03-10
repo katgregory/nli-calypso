1cb3b2c22bc7b0b4c733af7f9cb1f77e8b6b1859
Kenneth Xu <kennethxu@DN0a226397.SUNet>
2017-03-09 20:48:36 -0800

    started validation code

--- code/train.py
+++ code/train.py
@@ -15,11 +15,15 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-tf.app.flags.DEFINE_float("learning_rate", 0.01, "Learning rate.")
-tf.app.flags.DEFINE_float("max_gradient_norm", 10.0, "Clip gradients to this norm.")
-tf.app.flags.DEFINE_float("dropout", 0.15, "Fraction of units randomly dropped on non-recurrent connections.")
+# HYPERPARAMETERS
+tf.app.flags.DEFINE_float("lr", 0.0001, "Learning rate.")
+tf.app.flags.DEFINE_float("dropout_keep", 0.8, "Keep_prob")
+tf.app.flags.DEFINE_float("reg_lambda", 0.01, "Regularization")
+
 tf.app.flags.DEFINE_integer("batch_size", 64, "Batch size to use during training.")
 tf.app.flags.DEFINE_integer("epochs", 10, "Number of epochs to train.")
+
+tf.app.flags.DEFINE_float("max_gradient_norm", 10.0, "Clip gradients to this norm.")
 tf.app.flags.DEFINE_integer("state_size", 200, "Size of each model layer.")
 tf.app.flags.DEFINE_integer("output_size", 3, "The output size of your model.")
 tf.app.flags.DEFINE_integer("embedding_size", 300, "Size of the pretrained vocabulary.")
@@ -33,6 +37,7 @@ tf.app.flags.DEFINE_integer("keep", 0, "How many checkpoints to keep, 0 indicate
 tf.app.flags.DEFINE_string("vocab_path", "data/snli/vocab.dat", "Path to vocab file (default: ./data/snli/vocab.dat)")
 tf.app.flags.DEFINE_string("embed_path", "", "Path to the trimmed GLoVe embedding (default: ./data/snli/glove.trimmed.{embedding_size}.npz)")
 tf.app.flags.DEFINE_float("num_classes", 3, "Neutral, Entailment, Contradiction")
+tf.app.flags.DEFINE_float("optimize_hyperparameters", True, "Whether to optimize hyperparameters or not")
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -107,29 +112,46 @@ def main(_):
       embeddings = embeddings_dict['glove']
       vocab, rev_vocab = initialize_vocab(vocab_path)
 
-      # Initalize the NLI System
-      premise = Statement()
-      hypothesis = Statement()
-      nli = NLISystem(embeddings, premise, hypothesis, len(vocab), FLAGS.embedding_size, FLAGS.num_classes)
+      def run_model(lr, dropout_keep, reg_lambda):
+        # Initalize the NLI System
+        premise = Statement()
+        hypothesis = Statement()
+        nli = NLISystem(embeddings, premise, hypothesis, len(vocab), FLAGS.embedding_size, FLAGS.num_classes, lr, dropout_keep, reg_lambda)
 
-      if not os.path.exists(FLAGS.log_dir):
-        os.makedirs(FLAGS.log_dir)
-        file_handler = logging.FileHandler(pjoin(FLAGS.log_dir, "log.txt"))
-        logging.getLogger().addHandler(file_handler)
+        if not os.path.exists(FLAGS.log_dir):
+          os.makedirs(FLAGS.log_dir)
+          file_handler = logging.FileHandler(pjoin(FLAGS.log_dir, "log.txt"))
+          logging.getLogger().addHandler(file_handler)
 
-      print(vars(FLAGS))
-      with open(os.path.join(FLAGS.log_dir, "flags.json"), 'w') as fout:
-        json.dump(FLAGS.__flags, fout)
+        # print(vars(FLAGS))
+        with open(os.path.join(FLAGS.log_dir, "flags.json"), 'w') as fout:
+          json.dump(FLAGS.__flags, fout)
 
-      with tf.Session() as sess:
-        initialize_model(sess, nli, FLAGS.load_train_dir)
+        with tf.Session() as sess:
+          initialize_model(sess, nli, FLAGS.load_train_dir)
 
-        # Train the model
-        nli.train(sess, train_dataset, rev_vocab, FLAGS.train_dir, FLAGS.batch_size)
+          # Train the model
+          epoch_number, train_accuracy, train_loss = nli.train(sess, train_dataset, rev_vocab, FLAGS.train_dir, FLAGS.batch_size)
 
-        # Evaluate on the dev set
+          # Evaluate on the dev set
+          test_accuracy, avg_test_loss, cm = nli.evaluate_prediction(sess, test_dataset)
 
-        nli.evaluate_prediction(sess, test_dataset)
+          return (epoch_number, train_accuracy, train_loss, test_accuracy, avg_test_loss, cm)
+
+      # RUN THE MODEL
+      if not FLAGS.optimize_hyperparameters:
+        run_model(FLAGS.lr, FLAGS.dropout_keep, FLAGS.reg_lambda)
+      else:
+        lr_range = np.array([10**lr_exp for lr_exp in range(-8, 0, 1)])
+        dropout_range = np.arange(0.1, 1.1, 0.1) 
+        reg_lambda_range = np.array([10**lr_exp for lr_exp in range(-4, 0, 1)]) 
+        results_grid = np.empty(shape=[len(lr_range), len(dropout_range), len(reg_lambda)])
+        for x, lr in enumerate(lr_range):
+          for y, dropout_keep in enumerate(dropout_range):
+            for z, reg_lambda in enumerate(reg_lambda_range):
+              results_grid[x, y, z] = run_model(lr, dropout_keep, reg_lambda)
+
+
 
 if __name__ == "__main__":
   tf.app.run()
