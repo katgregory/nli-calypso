270bc35736eabba79c66134a7d65ae76d195b877
Kenneth Xu <kennethxu@rescomp-16-315108.stanford.edu>
2017-03-11 16:03:22 -0800

    alksdfjlaskdjf

--- code/processPickle.py
+++ code/processPickle.py
@@ -2,9 +2,10 @@ import cPickle as pickle
 import os
 from matplotlib import cm
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D, axes3d
 import numpy as np
 import matplotlib.ticker as mticker
+import matplotlib.patches as mpatches
 
 
 file_path = 'gpu-runs/run2/grid.p'
@@ -57,17 +58,51 @@ for key in keys:
 def log_tick_formatter(val, pos=None):
     return "{:.2e}".format(10**val)
 
+
+# sp = ax.scatter(np.log10(lr), dropout_keep, test_accuracy, c=reg_lambda, cmap=cm.coolwarm)
+# sp = ax.scatter(np.log10(lr), dropout_keep, test_accuracy)
+
+# PROCESS DATA FOR PLOTTING
+lr = np.log10(lr)
+graph_map = {}
+for i in xrange(len(lr)):
+  graph_map[(lr[i], dropout_keep[i], reg_lambda[i])] = test_accuracy[i]
+
+X = np.unique(np.array(sorted(lr)))
+Y = np.unique(np.array(sorted(dropout_keep)))
+X, Y = np.meshgrid(X, Y)
+
+# For each reg_lambda
+Zs = []
+for reg_lam in reg_lambda:
+  Z = np.empty(np.shape(X))
+  for i in xrange(np.shape(Z)[0]):
+    for j in xrange(np.shape(Z)[1]):
+      Z[i, j] = graph_map[(X[i, j], Y[i, j], reg_lam)]
+  Zs.append(Z)
+
+colors = ["red", "blue", "green", "yellow", "purple", "orange", "black"]
+reg_lambda = np.array(np.unique(reg_lambda))
+
+#### PLOTTING CODE ###########
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-sp = ax.scatter(np.log10(lr), dropout_keep, test_accuracy, c=reg_lambda, cmap=cm.coolwarm)
+for i in xrange(len(reg_lambda)):
+  ax.plot_wireframe(X, Y, Zs[i], color=colors[i])
 
 ax.set_xlabel('lr')
 # lr is on a log scale
 ax.xaxis.set_major_formatter(mticker.FuncFormatter(log_tick_formatter))
 ax.set_ylabel('dropout_keep')
 ax.set_zlabel('test_accuracy')
+
+# CREATE LEGEND
+patches = []
+for i in xrange(len(reg_lambda)):
+  patches.append(mpatches.Patch(color=colors[i], label=str(reg_lambda[i])))
+plt.legend(handles=patches, title="Regularization Constant")
 # Color is reg_lambda
-fig.colorbar(sp, shrink=0.5, aspect=5)
+# fig.colorbar(sp, shrink=0.5, aspect=5)
 
 plt.title("Hyperparameter Grid Search")
 plt.savefig('./data/hyperparams/' + output_file_name + '.png')
