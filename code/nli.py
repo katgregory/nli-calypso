87780bdd0e69bf1fb2102a4147b4060aed3baf74
Kenneth Xu <kennethxu@rescomp-16-315108.stanford.edu>
2017-03-16 02:32:49 -0700

    He left it - dead - and with its head,

--- code/nli.py
+++ code/nli.py
@@ -142,13 +142,13 @@ class NLI(object):
       if weight_attention:
         # Reshape to 2D matrices for the first multiplication
         W = tf.get_variable("W", shape=(hidden_size, hidden_size), initializer=xavier())
-        W = tf.clip_by_value(W, clip_value_min=-80, clip_value_max=80)
         statement1_len = tf.shape(states1)[1]
 
         # states1: batch_size * statement1_len x hidden_size
         # e: batch_size * statement1_len x hidden_size
         states1 = tf.reshape(states1, (batch_size * statement1_len, hidden_size))
         e = tf.matmul(states1, W)
+        e = tf.clip_by_value(e, clip_value_min=-80, clip_value_max=80)
 
         # Reshape to 3D matrices for the second multiplication
         # states1: batch_size x statement1_len x hidden_size
